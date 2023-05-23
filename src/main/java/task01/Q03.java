dc266ae98904517b44113976d2ad69620a228aef
Cihan Bagriyanik <cihanbagriyanikde@gmail.com>
2023-05-23 23:16:58 +0200

    deneme

---
+++ src/main/java/task01/Q03.java
@@ -0,0 +1,23 @@
+package task01;
+
+import java.util.Scanner;
+
+public class Q03 {
+
+	// TASK Q03 :
+	// TASK : Kullanıcıdan alacağınız bir stringde boşluk karakterinin olup olmadığını kontrol ediniz.
+	// TASK : Kullanıcıdan alacağınız bir stringde boşluk karakterinin olup olmadığını kontrol ediniz.
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		System.out.println("Lutfen bir cumle giriniz...");
+		String data = input.nextLine();
+
+		boolean result = data.contains(" ") ;
+		System.out.println("Bosluk olma durumu = " + result);
+
+
+
+	}
+}
